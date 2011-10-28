8b35617b38dd61a472837bb196a08b2af2c73952
Jens Ayton <jens@ayton.se>
2011-10-28 17:06:33 +0200

    NBTProdder can now add tags.

--- NBTProdder/NBTProdder/NBTDocument.m
+++ NBTProdder/NBTProdder/NBTDocument.m
@@ -1,10 +1,27 @@
-//
-//  JADocument.m
-//  NBTProdder
-//
-//  Created by Jens Ayton on 2011-10-27.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
+/*
+	NBTDocument.m
+	
+	
+	Copyright © 2011 Jens Ayton
+	
+	Permission is hereby granted, free of charge, to any person obtaining a
+	copy of this software and associated documentation files (the “Software”),
+	to deal in the Software without restriction, including without limitation
+	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+	and/or sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+	DEALINGS IN THE SOFTWARE.
+*/
 
 #import "NBTDocument.h"
 #import "NBTItem.h"
@@ -17,6 +34,8 @@
 
 @property (weak) IBOutlet NSOutlineView *outlineView;
 @property (strong) IBOutlet NSTreeController *treeController;
+
+- (IBAction) addItem:(id)sender;
 
 @end
 
@@ -70,6 +89,66 @@
 
 + (BOOL) autosavesInPlace
 {
+	return YES;
+}
+
+
+- (BOOL) canAddItem
+{
+	NSArray *selected = self.treeController.selectedObjects;
+	if (selected.count != 1)  return NO;
+	
+	NBTItem *selItem = [selected objectAtIndex:0];
+	if (selItem.parent == nil)
+	{
+		// Item is root; only allow adding if collection.
+		JANBTTagType type = selItem.type;
+		return type == kJANBTTagCompound || type == kJANBTTagList;
+	}
+	
+	return YES;
+}
+
+
+- (IBAction) addItem:(id)sender
+{
+	if (!self.canAddItem)  return;
+	
+	NBTItem *selected = [self.treeController.selectedObjects objectAtIndex:0];
+	NSIndexPath *selPath = self.treeController.selectionIndexPath;
+	
+	NBTItem *new = [NBTItem new];
+	new.name = NSLocalizedString(@"new", @"New compound member name.");
+	
+	JANBTTagType type = selected.type;
+	
+	if (type == kJANBTTagList)
+	{
+		// Insert at end of list.
+		new.type = selected.elementType;
+		selPath = [selPath indexPathByAddingIndex:selected.children.count];
+	}
+	else if (type == kJANBTTagCompound)
+	{
+		// Insert at end of compound.
+		new.type = kJANBTTagInt;
+		selPath = [selPath indexPathByAddingIndex:selected.children.count];
+	}
+	else
+	{
+		new.type = type;
+	}
+	[self.treeController insertObject:new atArrangedObjectIndexPath:selPath];
+}
+
+
+- (BOOL) validateUserInterfaceItem:(id <NSValidatedUserInterfaceItem>)item
+{
+	SEL action = item.action;
+	if (action == @selector(addItem:))
+	{
+		return self.canAddItem;
+	}
 	return YES;
 }
 
