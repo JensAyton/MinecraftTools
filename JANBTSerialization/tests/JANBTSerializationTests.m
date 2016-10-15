96ba082023b805105acfa079bed531d7e008537c
Jens Ayton <jens@ayton.se>
2016-10-15 16:51:10 +0200

    Add more complex NBT round-trip test

--- JANBTSerialization/tests/JANBTSerializationTests.m
+++ JANBTSerialization/tests/JANBTSerializationTests.m
@@ -1,6 +1,8 @@
 #import <XCTest/XCTest.h>
 
 #import "JANBTSerialization.h"
+#import "JAZLibCompressor.h"
+#import "JANBTTagType.h"
 
 @interface JANBTSerializationTests : XCTestCase
 
@@ -41,6 +43,8 @@
 	XCTAssertEqualObjects(rootName, @"hello world");
 	XCTAssertEqualObjects(root, @{ @"name": @"Bananrama" });
 }
+
+static NSString * const byteArrayTestKey = @"byteArrayTest (the first 1000 values of (n*n*255+n*7)%100, starting with n=0 (0, 62, 34, 16, 8, ...))";
 
 - (void)testReadComplexTest
 {
@@ -86,7 +90,7 @@
 				@"created-on": @1264099775885LL,
 			},
 		],
-		@"byteArrayTest (the first 1000 values of (n*n*255+n*7)%100, starting with n=0 (0, 62, 34, 16, 8, ...))": self.bigTestBytes,
+		byteArrayTestKey: self.bigTestBytes,
 		@"doubleTest": @0.4931287132182315
 	};
 
@@ -117,7 +121,39 @@
 	NSData *reencoded = [JANBTSerialization dataWithNBTObject:root rootName:rootName options:JANBTWritingOptionsUncompressed schema:nil error:&error];
 
 	XCTAssertNil(error);
-	XCTAssertEqualObjects(testNBT, reencoded);
+	XCTAssertEqualObjects(testNBT, reencoded);	// NOTE: dictionary only contains one key, so this is OK.
+}
+
+- (void)testRoundtripBigTest
+{
+	NSData *testNBT = [self NBTWithName:@"bigtest"];
+
+	NSString *rootName;
+	id root = [JANBTSerialization NBTObjectWithData:testNBT rootName:&rootName options:0 schema:nil error:nil];
+
+	NSError *error;
+	NSData *reencoded = [JANBTSerialization dataWithNBTObject:root rootName:rootName options:0 schema:nil error:&error];
+	XCTAssertNil(error);
+
+	// Because it's a dictionary, reencoding may have reordered contents. Decoding should provide the same data, though.
+	NSDictionary *newRoot = [JANBTSerialization NBTObjectWithData:reencoded rootName:&rootName options:0 schema:nil error:&error];
+
+	XCTAssertNil(error);
+	XCTAssertEqualObjects(root, newRoot);
+
+	XCTAssertEqual([newRoot[@"shortTest"] ja_NBTType], kJANBTTagShort);
+	XCTAssertEqual([newRoot[@"longTest"] ja_NBTType], kJANBTTagLong);
+	XCTAssertEqual([newRoot[@"floatTest"] ja_NBTType], kJANBTTagFloat);
+	XCTAssertEqual([newRoot[@"stringTest"] ja_NBTType], kJANBTTagString);
+	XCTAssertEqual([newRoot[@"intTest"] ja_NBTType], kJANBTTagInt);
+	XCTAssertEqual([newRoot[@"nested compound test"] ja_NBTType], kJANBTTagCompound);
+	XCTAssertEqual([newRoot[@"listTest (long)"] ja_NBTType], kJANBTTagList);
+	XCTAssertEqual([newRoot[@"listTest (long)"] ja_NBTListElementType], kJANBTTagLong);
+	XCTAssertEqual([newRoot[@"byteTest"] ja_NBTType], kJANBTTagByte);
+	XCTAssertEqual([newRoot[@"listTest (compound)"] ja_NBTType], kJANBTTagList);
+	XCTAssertEqual([newRoot[@"listTest (compound)"] ja_NBTListElementType], kJANBTTagCompound);
+	XCTAssertEqual([newRoot[byteArrayTestKey] ja_NBTType], kJANBTTagByteArray);
+	XCTAssertEqual([newRoot[@"doubleTest"] ja_NBTType], kJANBTTagDouble);
 }
 
 @end
